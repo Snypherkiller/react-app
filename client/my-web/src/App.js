f17d405fafc86687cfd8c1122d052f5d81661eb6
Snypherkiller <159802996+Snypherkiller@users.noreply.github.com>
2024-07-25 22:40:11 +0530

    cc

--- client/my-web/src/App.js
+++ client/my-web/src/App.js
@@ -11,7 +11,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/Snypherkiller/Electronics-inventory-control-system"
           target="_blank"
           rel="noopener noreferrer"
         >
