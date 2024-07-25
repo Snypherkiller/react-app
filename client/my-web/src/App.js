e7c630cfc07688de329dd70841a4b067e7920111
Snypherkiller <159802996+Snypherkiller@users.noreply.github.com>
2024-07-25 22:37:02 +0530

    commit

--- client/my-web/src/App.js
+++ client/my-web/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React hello mr dunith
+          Welcome to the new react project
         </a>
       </header>
     </div>
