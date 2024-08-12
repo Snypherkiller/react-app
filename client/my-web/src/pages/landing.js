0cf4d1b705697c02e1a5fdf54088a360a409cfd8
Snypherkiller <159802996+Snypherkiller@users.noreply.github.com>
2024-08-12 21:45:28 +0530

    dd

--- client/my-web/src/pages/landing.js
+++ client/my-web/src/pages/landing.js
@@ -1,13 +1,15 @@
-import logo from '../assets/images/logo.svg'
-import main from '../assets/images/main.svg'
+import main from '../assets/images/proud.svg.svg'
 import styled from 'styled-components'
 import Wrapper from '../assets/wrappers/Testing'
+import Logo from '../components/Logo' 
+
+
 const Landing = () => {
   return (
     <Wrapper>
     <div>
       <nav>
-        <img src={logo} alt="jobify" className='logo'/>
+        <Logo/>
       </nav>
       <div className="container page">
         <div className='info'>
